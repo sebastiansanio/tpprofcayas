e8955b337bb41fa435bbae1c46e74e6c1c7601b2
seba90s <seba90s@eb6ec705-e1d5-5cfc-520b-f8c4e66f68df>
2012-10-28 04:49:57 +0000

    Shiro.

---
+++ grails-app/domain/login/User.groovy
@@ -0,0 +1,12 @@
+package login
+
+class User {
+    String username
+    String passwordHash
+    
+    static hasMany = [ roles: Role, permissions: String ]
+
+    static constraints = {
+        username(nullable: false, blank: false, unique: true)
+    }
+}
