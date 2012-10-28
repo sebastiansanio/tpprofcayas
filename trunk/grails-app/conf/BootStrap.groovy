73c872e9e4099525c07e0dd51ae22f319e14831e
seba90s <seba90s@eb6ec705-e1d5-5cfc-520b-f8c4e66f68df>
2012-10-28 04:49:57 +0000

    Shiro.

--- trunk/grails-app/conf/BootStrap.groovy
+++ trunk/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import org.apache.shiro.crypto.hash.Sha256Hash
+import login.*
+
 class BootStrap {
 
     def init = { servletContext ->
+		def user = new User(username:"cayas", passwordHash: new Sha256Hash("cayas").toHex())
+		user.addToPermissions("*:*")
+		user.save()
     }
     def destroy = {
     }
