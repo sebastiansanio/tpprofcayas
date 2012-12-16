6a45614a3b18ca86ecc7229554731f12917b08d8
seba90s <seba90s@eb6ec705-e1d5-5cfc-520b-f8c4e66f68df>
2012-12-16 02:29:59 +0000

    Clase pedido con todos los atributos.

--- trunk/test/unit/wish/PaymentStatusTests.groovy
+++ trunk/test/unit/wish/PaymentStatusTests.groovy
@@ -8,8 +8,8 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(LoadStatus)
-class LoadStatusTests {
+@TestFor(PaymentStatus)
+class PaymentStatusTests {
 
     void testSomething() {
        fail "Implement me"
