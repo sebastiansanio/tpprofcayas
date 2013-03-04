545b047277c9cbd2b8689ae0a23c37e7b938020b
seba90s <seba90s@eb6ec705-e1d5-5cfc-520b-f8c4e66f68df>
2013-03-04 23:28:57 +0000

    Contraints de la clase Document.

--- grails-app/domain/wish/Document.groovy
+++ grails-app/domain/wish/Document.groovy
@@ -7,11 +7,16 @@ class Document {
 	DocumentType documentType
 	Date received
 	String trackingNumber
+	byte[] file
 	
     static mapping = {
     }
     
 	static constraints = {
+		file size: 1..9999999,nullable:true
+		received nullable:true
+		trackingNumber nullable:true,blank:true
+		
     }
 	
 
