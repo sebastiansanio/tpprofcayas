8269f07ba8792ca8f31ea86cd92ddd41eb934dee
seba90s <seba90s@eb6ec705-e1d5-5cfc-520b-f8c4e66f68df>
2013-08-05 01:13:30 +0000

    Export de alertas para clientes, proveedores, etc.

--- grails-app/controllers/alert/AlertExternalController.groovy
+++ grails-app/controllers/alert/AlertExternalController.groovy
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.support.RequestContextUtils
 class AlertExternalController {
 
 	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd")
+	private static final DateFormat DATE_FORMAT_SOURCE = new SimpleDateFormat("dd/MM/yyyy")
 		
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
@@ -21,21 +22,18 @@ class AlertExternalController {
         redirect(action: "list", params: params)
     }
 	
-
     def list() {
 		params.max = Math.min(params.max ? params.int('max') : 200, 1000)
 		params.sort = params.sort?: 'attentionDate'
 		params.sort = params.sort?: 'asc'
+		User user = User.findByUsername(SecurityUtils.subject.principal)
 		
 		def alerts = new ArrayList()
 		def alertsQuantity = 0
-		
-		User user = User.findByUsername(SecurityUtils.subject.principal)
 		def alertTypes = AlertType.list().findAll{
-			user?.stakeholder.type in it.stakeholders
+			user?.stakeholder?.type in it.stakeholders
 		}
 		def wishes = user?.stakeholder?.wishes
-		
 		if(alertTypes.size()>0 && wishes.size()>0){
 			alerts = Alert.findAllByDateFinalizedIsNullAndWishInListAndAlertTypeInList(wishes,alertTypes,params)
 			alertsQuantity = Alert.countByDateFinalizedIsNullAndWishInListAndAlertTypeInList(wishes,alertTypes)
@@ -44,5 +42,17 @@ class AlertExternalController {
         [alertInstanceList: alerts, alertInstanceTotal: alertsQuantity,user:user]
     }
 	
+	def query() {
+		[:]
+	}
+	
+	def export() {
+		Date fromDate = DATE_FORMAT_SOURCE.parse(params.fromDate)
+		Date toDate = DATE_FORMAT_SOURCE.parse(params.toDate)
+		User user = User.findByUsername(SecurityUtils.subject.principal)
+		response.contentType=grailsApplication.config.grails.mime.types[params.format]
+		response.setHeader("Content-disposition", "attachment;filename=${message(code:'alerts.label')} "+DATE_FORMAT.format(new Date())+".${params.extension}")
+		alertExportService.exportAlertsByStakeholderAndDate(params.format,response.outputStream,user.stakeholder,RequestContextUtils.getLocale(request),fromDate,toDate)
+	}
 
 }
--- grails-app/services/alert/AlertExportService.groovy
+++ grails-app/services/alert/AlertExportService.groovy
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Locale;
 
 import org.springframework.context.MessageSource;
+import stakeholder.Stakeholder
 
 
 class AlertExportService {
@@ -14,7 +15,7 @@ class AlertExportService {
 	def exportService
 	MessageSource messageSource
 
-    def exportAlert(String format,OutputStream outputStream,Locale locale,Date fromDate,Date toDate,boolean pendingOnly) {
+	def exportAlert(String format,OutputStream outputStream,Locale locale,Date fromDate,Date toDate,boolean pendingOnly) {
 		
 		List alerts = Alert.findAllByAttentionDateGreaterThanEqualsAndAttentionDateLessThanEquals(fromDate,toDate,[sort:"attentionDate"])
 		
@@ -30,7 +31,6 @@ class AlertExportService {
 		List fields = ["alertType","attentionDate","deadline","wish.customer","wish.supplier",
 			"wish.supplierOrder","wish.opNumber","dateFinalized","finalizedReason","lastInspected"]
 		
-		
 		Map labels = ["alertType":messageSource.getMessage("alert.alertType.label",null,locale),
 			"attentionDate":messageSource.getMessage("alert.attentionDate.label",null,locale),
 			"deadline":messageSource.getMessage("alert.deadline.label",null,locale),
@@ -44,15 +44,44 @@ class AlertExportService {
 		Map formatters = ["attentionDate":dateFormatter,"deadline":dateFormatter,
 			"dateFinalized":dateFormatter,"lastInspected":dateFormatter]		
 		
+		Map parameters = [title: messageSource.getMessage("alerts.label",null,locale)]
+		exportService.export(format,outputStream,alerts,fields,labels,formatters,parameters)
+					
+    }		
+	
+	def exportAlertsByStakeholderAndDate(String format,OutputStream outputStream,Stakeholder stakeholder,Locale locale,Date fromDate,Date toDate){
+		def alerts = new ArrayList()
+		def alertsQuantity = 0
+		def alertTypes = AlertType.list().findAll{
+			stakeholder?.type in it.stakeholders
+		}
+		def wishes = stakeholder?.wishes
+		if(alertTypes.size()>0 && wishes.size()>0){
+			alerts = Alert.findAllByDateFinalizedIsNullAndWishInListAndAlertTypeInList(wishes,alertTypes,[sort:"attentionDate"])	
+		}		
 		
+		def dateFormatter = {domain, value->
+			return value?.format("dd/MM/yyyy")
+		}
 
+		List fields = ["alertType.externalMessage","attentionDate","deadline"]
+		for (field in ["customer","supplier","supplierOrder","customerOpNumber","opNumber"]){
+			if(stakeholder?.defaultReport?.fields?.contains(field))
+				fields.add("wish."+field)
+		}
+		Map labels = ["alertType.externalMessage":messageSource.getMessage("alertType.label",null,locale),
+			"attentionDate":messageSource.getMessage("alert.attentionDate.label",null,locale),
+			"deadline":messageSource.getMessage("alert.deadline.label",null,locale),
+			"wish.customer":messageSource.getMessage("wish.customer.label",null,locale),
+			"wish.supplier":messageSource.getMessage("wish.supplier.label",null,locale),
+			"wish.supplierOrder":messageSource.getMessage("wish.supplierOrder.label",null,locale),
+			"wish.customerOpNumber":messageSource.getMessage("wish.customerOpNumber.label",null,locale),
+			"wish.opNumber":messageSource.getMessage("wish.opNumber.label",null,locale)]
+		Map formatters = ["attentionDate":dateFormatter,"deadline":dateFormatter]
+		
 		Map parameters = [title: messageSource.getMessage("alerts.label",null,locale)]
 		exportService.export(format,outputStream,alerts,fields,labels,formatters,parameters)
 		
-				
-    }
-	
-			
-			
-		
+	}
+
 }
